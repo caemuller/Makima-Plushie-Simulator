7d05208899f2637537ed7f0ac0d6c8fe5069d09a
Victorakachubinha <victorelitt@gmail.com>
2023-08-29 19:14:53 -0300

    intersec fixed

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -4,6 +4,10 @@ bool intersection_hitsphere(glm::vec4 pos,glm::vec4 aim, glm::vec4 model_center)
     float C = norm(pos - model_center) * norm(pos - model_center) - 10;
 
     float t;
-    t = t*t*A + t*B + C;
-    return false;
+    if(0 == t*t*A + t*B + C){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
